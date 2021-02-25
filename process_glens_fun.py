b45b401c2fd2107375f02ed00191190d2b1a2f14
Daniel Hueholt <dmhuehol@ncsu.edu>
2021-02-25 11:39:14 -0700

    Filename parsing
    Pull useful information out of GLENS filenames

--- process_glens_fun.py
+++ process_glens_fun.py
@@ -2,6 +2,30 @@ import xarray as xr
 import numpy as np
 import sys
 
+# baselineFname = baselineStr[len(baselinePath):len(baselineStr)] #only filename
+def extract_meta_from_fname(fname):
+# Extract useful metadata from GLENS output filename
+    # baselineFname = fname[len(fname):len(fname)] #only filename
+    fnamePcs = fname.split('.') #period is delimiter in filename
+    glensMeta = {"unkn1": fnamePcs[0],
+        "unkn2": fnamePcs[1],
+        "unkn3": fnamePcs[2],
+        "unkn4": fnamePcs[3],
+        "runType": fnamePcs[4],
+        "ensMemNum": int(fnamePcs[5]),
+        "unkn5": fnamePcs[6],
+        "unkn6": fnamePcs[7],
+        "dataVar": fnamePcs[8],
+        "rawDates": fnamePcs[9],
+        "strtYr": int(fnamePcs[9][0:4]),
+        "strtMnth": int(fnamePcs[9][4:6]),
+        "endYr": int(fnamePcs[9][7:11]),
+        "endMnth": int(fnamePcs[9][11:13]),
+        "extension": fnamePcs[10]
+    }
+
+    return glensMeta
+
 def discover_data_var(glensDsetCntrl):
     fileKeys = list(glensDsetCntrl.keys())
     notDataKeys = ['time_bnds', 'date', 'datesec']
