533df61e067cd62957358b2e6abf5f44db707d8b
Daniel Hueholt <dmhuehol@ncsu.edu>
2021-03-11 14:02:13 -0700

    Implement (clumsily) region and point plotting
    Regional and point plotting code works as implemented! Now need to allow for all lat/lon inputs to go through the latOfInt/lonOfInt interface and automate the figure title generation.

--- plot_decadalpdf_script_libbyformat.py
+++ plot_decadalpdf_script_libbyformat.py
@@ -20,15 +20,16 @@ cntrlFile = '/Users/dhueholt/Documents/ANN_GeoEng/data/GLENS/control.001.SST.r90
 fdbckFile = '/Users/dhueholt/Documents/ANN_GeoEng/data/GLENS/feedback.001.SST.r90x45.shift.annual.nc'
 
 baselineFlag = 1
-savePath = '/Users/dhueholt/Documents/GLENS_fig/20210301_regiontesting/'
-saveName = 'pdf_SSTglobal_cntrlfdbck_TESTREGION'
+savePath = '/Users/dhueholt/Documents/GLENS_fig/20210311_regiontesting/'
+saveName = 'pdf_SST_cntrlfdbck_TESTPOINT'
 dpi_val = 300
 
 levOfInt = 500 #hPa
-latOfInt = 34
-lonOfInt = -78
+# latOfInt = np.array([19.5,30])
+latOfInt = -28.6
+lonOfInt = 112
 quantileForFig = 0.66
-regionToPlot = 'global' #aspirational
+regionToPlot = 'point' #aspirational
 
 cntrlIntToPlot = [2010,2020,2030,2040,2050,2090]
 fdbckIntToPlot = [2020,2030,2040,2050,2090]
@@ -46,33 +47,38 @@ if regionToPlot == 'global':
     fdbckDsetLoi = fdbckDset.sel(z_t=levOfInt) #z_t is the equivalent to level, I guess?
     fdbckDarr = fdbckDsetLoi[dataKey]
     fdbckDarrMnSpc = fdbckDarr.mean(dim=['lat','lon'])
+
+elif regionToPlot == 'regional':
+    cntrlDsetLoi = cntrlDset.sel(z_t=levOfInt)
+    dataKey = pgf.discover_data_var(cntrlDset)
+    cntrlDarrLoi = cntrlDsetLoi[dataKey]
+    lats = cntrlDsetLoi['lat'] #feedback and control are on same grid, fortunately
+    lons = cntrlDsetLoi['lon']
+    latMask = (lats>19.5) & (lats<30)
+    lonMask = (lons>263) & (lons<280)
+    cntrlDarrLoiAoi = cntrlDarrLoi[:,latMask,lonMask]
+    cntrlDarrMnSpc = cntrlDarrLoiAoi.mean(dim=['lat','lon'])
+
+    fdbckDsetLoi = fdbckDset.sel(z_t=levOfInt) #z_t is the equivalent to level, I guess?
+    dataKey = pgf.discover_data_var(fdbckDset)
+    fdbckDarrLoi = fdbckDsetLoi[dataKey]
+    fdbckDarrLoiAoi = fdbckDarrLoi[:,latMask,lonMask]
+    fdbckDarrMnSpc = fdbckDarrLoiAoi.mean(dim=['lat','lon'])
+
+elif regionToPlot == 'point':
+    cntrlDsetLoiPoi = cntrlDset.sel(z_t=levOfInt, lat=latOfInt, lon=lonOfInt, method="nearest")
+    dataKey = pgf.discover_data_var(cntrlDset)
+    cntrlDarrLoiPoi = cntrlDsetLoiPoi[dataKey]
+    cntrlDarrMnSpc = cntrlDarrLoiPoi #need better variable name
+
+    fdbckDsetLoiPoi = fdbckDset.sel(z_t=levOfInt, lat=latOfInt, lon=lonOfInt, method="nearest")
+    dataKey = pgf.discover_data_var(fdbckDset)
+    fdbckDarrLoiPoi = fdbckDsetLoiPoi[dataKey]
+    fdbckDarrMnSpc = fdbckDarrLoiPoi
+
 else:
-    print("Not TODAY")
-    # cntrlDsetLoi = cntrlDset.sel(z_t=levOfInt) #z_t is the equivalent to level, I guess?
-    # dataKey = pgf.discover_data_var(cntrlDset)
-    # cntrlDsetLoi = cntrlDsetLoi[dataKey]
-    # print(cntrlDsetLoi)
-    # latCondition = np.where((cntrlDsetLoi['lat']>19.5) & (cntrlDsetLoi['lat']<30))[0]
-    # print(latCondition)
-    # cntrlDsetLoiLat = cntrlDsetLoi[latCondition]
-    # lonCondition = np.where((cntrlDsetLoi['lon']>-97) & (cntrlDsetLoi['lon']<-80))[0]
-    # cntrlDarr = cntrlDsetLoiLat[lonCondition]
-    # cntrlDarrMnSpc = cntrlDarr.mean(dim=['lat','lon'])
-    #
-    # fdbckDsetLoi = fdbckDset.sel(z_t=levOfInt) #z_t is the equivalent to level, I guess?
-    # dataKey = pgf.discover_data_var(fdbckDset)
-    # fdbckDsetLoi = fdbckDsetLoi[dataKey]
-    # print(fdbckDsetLoi)
-    # # latCondition2 = np.where((fdbckDsetLoi['lat']>19.5) & (fdbckDsetLoi['lat']<30))[0]
-    # # print(latCondition2)
-    # import sys
-    # sys.exit()
-    # fdbckDsetLoiLat = fdbckDsetLoi[latCondition]
-    # # lonCondition = np.where((fdbckDsetLoi['lon']>-97) & (fdbckDsetLoi['lon']<-80))
-    # fdbckDarr = fdbckDsetLoiLat[lonCondition]
-    # fdbckDarrMnSpc = fdbckDarr.mean(dim=['lat','lon'])
-    # print(fdbckDarrMnSpc)
-# print(cntrlDarrMnSpc)
+    print("Invalid region!")
+
 
 # take 2010-2019 average and remove this
 # baselineMeanToRmv = dot.average_over_years(cntrlDarrMnSpc,2010,2019)
@@ -96,7 +102,7 @@ colorsToPlot = plt_tls.select_colors(baselineFlag,len(cntrlIntToPlot)-1,len(fdbc
 labelsToPlot = list(['2010-2019 Baseline'])
 labelsToPlot = plt_tls.generate_labels(labelsToPlot, cntrlIntToPlot, timePeriod, 'RCP8.5')
 labelsToPlot = plt_tls.generate_labels(labelsToPlot, fdbckIntToPlot, timePeriod, 'SAI')
-labelsToPlot.append('Global SST PDFs in GLENS')
+labelsToPlot.append('(-28.6,112) SST PDFs in GLENS')
 
 print(colorsToPlot)
 plt_tls.plot_pdf_kdeplot(handlesToPlot, colorsToPlot, labelsToPlot, savePath, saveName)
