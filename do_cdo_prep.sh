ad8f962287c7b155247fec3fc9892dabaadccc11
Daniel Hueholt <dmhuehol@ncsu.edu>
2021-04-16 16:02:27 -0600

    Add CDO commands to do_cdo_prep.sh

--- do_cdo_prep.sh
+++ do_cdo_prep.sh
@@ -1,1 +1,6 @@
-cdo mergetime inCard mergeFile
+IN_CARD='/Users/dhueholt/Documents/GLENS_data/controlForCDO/test.nc'
+MERGE_FILE='/Users/dhueholt/Documents/GLENS_data/controlForCDO/testMergeShell.nc'
+SHIFT_FILE='/Users/dhueholt/Documents/GLENS_data/controlForCDO/shiftMergeShell.nc'
+
+cdo mergetime $IN_CARD $MERGE_FILE
+cdo shifttime,'-1days' $MERGE_FILE $SHIFT_FILE
