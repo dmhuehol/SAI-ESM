1b7353cdd59b1bd341a0c38bbd2d60090ce00a4b
Daniel Hueholt <dmhuehol@ncsu.edu>
2021-04-20 11:37:00 -0600

    Update do_cdo_prep to semi-functionality
    Automatically creates useful output filename and carries out CDO commands. Reads IN_PATH, IN_TOKEN, and OUT_PATH as input variables for ease of use. (The script can simply be called twice instead of having separate sections for feedback and control runs. Also makes ensemble member handling easier for now--just specify single member at the token.)

--- do_cdo_prep.sh
+++ do_cdo_prep.sh
@@ -1,6 +1,43 @@
-IN_CARD='/Users/dhueholt/Documents/GLENS_data/controlForCDO/test.nc'
-MERGE_FILE='/Users/dhueholt/Documents/GLENS_data/controlForCDO/testMergeShell.nc'
-SHIFT_FILE='/Users/dhueholt/Documents/GLENS_data/controlForCDO/shiftMergeShell.nc'
+### Input variables
+IN_PATH=$1 #Path to data
+IN_TOKEN=$2 #Token to match data files, e.g. "*.001.*.nc"
+OUT_PATH=$3 #Path to save files
 
-cdo mergetime $IN_CARD $MERGE_FILE
-cdo shifttime,'-1days' $MERGE_FILE $SHIFT_FILE
+### Script
+IN_CARD="$IN_PATH$IN_TOKEN"
+PATH_LENGTH=${#IN_PATH}
+echo $PATH_LENGTH #troubleshooting
+
+RUN_FNAMES=()
+RUN_TIMES=()
+RUN_ENSNUMS=()
+for f in $IN_CARD; do
+  ACTIVE_FNAME=${f:$PATH_LENGTH}
+  RUN_FNAMES+=( $ACTIVE_FNAME )
+  ACTIVE_TIME=$(echo $ACTIVE_FNAME | cut -d'.' -f10)
+  RUN_TIMES+=( $ACTIVE_TIME )
+  ACTIVE_ENSNUM=$(echo $ACTIVE_FNAME | cut -d'.' -f6)
+  RUN_ENSNUMS+=( $ACTIVE_ENSNUM )
+  RUN_TYPE=$(echo $ACTIVE_FNAME | cut -d'.' -f5)
+  RUN_VAR=$(echo $ACTIVE_FNAME | cut -d'.' -f9)
+done
+
+### Troubleshooting
+# echo ${RUN_FNAMES[@]}
+# echo ${RUN_TIMES[@]}
+# echo $RUN_TYPE
+# echo ${RUN_ENSNUMS[@]}
+# echo $RUN_VAR
+
+OUT_FNAME="${RUN_TYPE}_${RUN_ENSNUMS[1]}_${RUN_VAR}"
+echo $OUT_FNAME
+for t in ${RUN_TIMES[@]}; do
+  OUT_FNAME="${OUT_FNAME}_${t}"
+done
+echo $OUT_FNAME
+
+OUT_MERGE="${OUT_PATH}${OUT_FNAME}_merge.nc"
+OUT_SHIFT="${OUT_PATH}${OUT_FNAME}_shift.nc"
+
+cdo mergetime $IN_CARD $OUT_MERGE
+cdo shifttime,'-1days' $OUT_MERGE $OUT_SHIFT
