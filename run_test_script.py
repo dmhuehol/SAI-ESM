7fd7570984999b914352da04b481936137e19d60
Daniel Hueholt <dmhuehol@ncsu.edu>
2021-05-17 10:20:57 -0600

    gitignore

--- run_test_script.py
+++ run_test_script.py
@@ -1,141 +1,93 @@
-# This makes decadal pdfs for data that's in single-file format as opposed to decadal-file format.
-# This will be combined with plot_decadalpdf_script after completion.
-# Also, this makes SST plots
-
 import xarray as xr
 import matplotlib.pyplot as plt
-import seaborn as sn
 import cartopy
 import cartopy.crs as ccrs
 import cmocean
 import numpy as np
-import scipy.stats as stats
-from glob import glob
-import time
+import sys
 
 import difference_over_time as dot
 import plotting_tools as plt_tls
 import process_glens_fun as pgf
 
 # Inputs
-cntrlFile = '/Users/dhueholt/Documents/ANN_GeoEng/data/GLENS/control.001.SST.r90x45.shift.annual.nc'
-fdbckFile = '/Users/dhueholt/Documents/ANN_GeoEng/data/GLENS/feedback.001.SST.r90x45.shift.annual.nc'
+dataPath = '/Users/dhueholt/Documents/GLENS_data/annual_o3/'
+filenameCntrl = 'control_003_O3_201001-201912_202001-202912_203001-203912_204001-204912_205001-205912_206001-206912_207001-207912_208001-208912_209001-209912_annual.nc'
+filenameFdbck = 'feedback_003_O3_202001-202912_203001-203912_204001-204912_205001-205912_206001-206912_207001-207912_208001-208912_209001-209912_annual.nc'
+cntrlPath = dataPath + filenameCntrl
+fdbckPath = dataPath + filenameFdbck
 
-baselineFlag = 0
-savePath = '/Users/dhueholt/Documents/GLENS_fig/20210311_regiontesting/'
-saveName = 'pdf_kde_SST_cntrlfdbck_Leeuwin_10yr_NOMEANTEST'
-plotStyle = 'kde' #'kde'
-dpiVal = 400
+savePath = '/Users/dhueholt/Documents/GLENS_fig/20210429_refactoringAndOzone/'
+saveFile = 'timeseries_O3TropCO_'
+saveName = savePath + saveFile
+dpi_val = 400
 
-levOfInt = 500 #hPa
-latOfInt = np.array([-35,-22])
-lonOfInt = np.array([108,115])
-# latOfInt = -28.6
-# lonOfInt = 112
-quantileForFig = 0.66
-regionToPlot = 'regional' #aspirational
-titleStr = 'Leeuwin Current SST PDFs in GLENS'
+# levOfInt = 1000 #hPa
+latOfInt = 60.79
+lonOfInt = 210.0
+regionToPlot = 'point' #aspirational
 
-cntrlIntToPlot = [2020,2030,2040,2050,2090]#[2020,2030,2040,2050,2090]#[2020,2050]
-fdbckIntToPlot = [2020,2030,2040,2050,2090]#[2020,2030,2040,2050,2090]#[2020,2050]
-timePeriod = 10 #number of years, i.e. 10 = decade
+# Control
+glensDsetCntrl = xr.open_dataset(cntrlPath)
+dataKey = pgf.discover_data_var(glensDsetCntrl)
+glensDarrCntrl = glensDsetCntrl[dataKey]
 
-cntrlDset = xr.open_dataset(cntrlFile)
-fdbckDset = xr.open_dataset(fdbckFile)
+# Feedback
+glensDsetFdbck = xr.open_dataset(fdbckPath)
+glensDarrFdbck = glensDsetFdbck[dataKey]
 
-if regionToPlot == 'global':
-    cntrlDsetLoi = cntrlDset.sel(z_t=levOfInt) #z_t is the equivalent to level, I guess?
-    dataKey = pgf.discover_data_var(cntrlDset)
-    cntrlDarr = cntrlDsetLoi[dataKey]
-    cntrlDarrMnSpc = cntrlDarr.mean(dim=['lat','lon'])
+bndDct = pgf.find_matching_year_bounds(glensDarrCntrl, glensDarrFdbck)
+glensCntrlPoi = glensDarrCntrl[bndDct['cntrlStrtMtch']:bndDct['cntrlEndMtch']+1] #RANGES IN PYTHON ARE [)
+# print(glensCntrlPoi['lev'][len(glensCntrlPoi['lev'].data)-1-16])
+# print(len(glensCntrlPoi['lev'].data)-1-16)
+# sys.exit('Leveled')
 
-    fdbckDsetLoi = fdbckDset.sel(z_t=levOfInt) #z_t is the equivalent to level, I guess?
-    fdbckDarr = fdbckDsetLoi[dataKey]
-    fdbckDarrMnSpc = fdbckDarr.mean(dim=['lat','lon'])
+glensFdbckPoi = glensDarrFdbck[bndDct['fdbckStrtMtch']:bndDct['fdbckEndMtch']+1]
+# levOfInt = glensCntrlPoi['lev'].data[len(glensCntrlPoi['lev'].data)-1]
+levOfInt = glensCntrlPoi['lev'].data[len(glensCntrlPoi['lev'].data)-1-16]
+levs = glensCntrlPoi['lev'].data
+levMask = levs > levOfInt
+print(levOfInt)
+print(levMask)
+# print(glensFdbckPoi['lon'])
+# sys.exit('longituded')
 
-elif regionToPlot == 'regional':
-    cntrlDsetLoi = cntrlDset.sel(z_t=levOfInt)
-    dataKey = pgf.discover_data_var(cntrlDset)
-    cntrlDarrLoi = cntrlDsetLoi[dataKey]
-    lats = cntrlDsetLoi['lat'] #feedback and control are on same grid, fortunately
-    lons = cntrlDsetLoi['lon']
-    latMask = (lats>latOfInt[0]) & (lats<latOfInt[1])
-    # print(lats[latMask])
-    lonMask = (lons>lonOfInt[0]) & (lons<lonOfInt[1])
-    # print(lons[lonMask])
-    cntrlDarrLoiAoi = cntrlDarrLoi[:,latMask,lonMask]
-    # cshp = np.shape(cntrlDarrLoiAoi)
-    # print(cshp)
-    # cntrlDarrMnSpc = np.reshape(cntrlDarrLoiAoi, (cshp[0], cshp[1] * cshp[2]))
-    cntrlDarrMnSpc = cntrlDarrLoiAoi.stack(spc=("lat","lon"))
-    # cntrlDarrMnSpc = cntrlDarrLoiAoi.mean(dim=['lat','lon'])
+if regionToPlot == 'point':
+    # glensCntrlPoi = glensCntrlPoi.sum(dim='lev')#
+    # cntrlToPlot = glensCntrlPoi.sel(lev=levOfInt, lat=latOfInt, lon=lonOfInt, method="nearest")
+    cntrlToPlot = glensCntrlPoi[:,levMask,:,:]
+    cntrlToPlot = glensCntrlPoi.sel(lat=latOfInt, lon=lonOfInt, method="nearest")
+    cntrlToPlot = cntrlToPlot.sum(dim='lev')
+    # cntrlToPlot = glensCntrlPoi.sel(lat=latOfInt, lon=lonOfInt, method="nearest")
+    # glensFdbckPoi = glensFdbckPoi.sum(dim='lev')
+    # fdbckToPlot = glensFdbckPoi.sel(lev=levOfInt, lat=latOfInt, lon=lonOfInt, method="nearest")
+    fdbckToPlot = glensFdbckPoi[:,levMask,:,:]
+    # print(fdbckToPlot['lev'].data)
+    fdbckToPlot = glensFdbckPoi.sel(lat=latOfInt, lon=lonOfInt, method="nearest")
+    fdbckToPlot = fdbckToPlot.sum(dim='lev')
+    print(fdbckToPlot.data)
+    # fdbckToPlot = glensFdbckPoi.sel(lat=latOfInt, lon=lonOfInt, method="nearest")
 
-    fdbckDsetLoi = fdbckDset.sel(z_t=levOfInt) #z_t is the equivalent to level, I guess?
-    dataKey = pgf.discover_data_var(fdbckDset)
-    fdbckDarrLoi = fdbckDsetLoi[dataKey]
-    fdbckDarrLoiAoi = fdbckDarrLoi[:,latMask,lonMask]
-    # fshp = np.shape(fdbckDarrLoiAoi)
-    # fdbckDarrMnSpc = np.reshape(fdbckDarrLoiAoi, (fshp[0],fshp[1] * fshp[2]))
-    fdbckDarrMnSpc = fdbckDarrLoiAoi.stack(spc=("lat","lon"))
-    # fdbckDarrMnSpc = fdbckDarrLoiAoi.mean(dim=['lat','lon'])
+# Plotting
+yStr = glensDarrFdbck.units
+varStr = glensDarrFdbck.long_name
+startStr = str(bndDct['strtYrMtch'])
+endStr = str(bndDct['endYrMtch'])
+levStr = str(np.round_(levOfInt,decimals=1))
+latStr = str(np.round_(cntrlToPlot.lat.data,decimals=2))
+lonStr = str(np.round_(cntrlToPlot.lon.data,decimals=2))
+locStr = latStr + '_' + lonStr
+locTitleStr = '(' + latStr + ',' + lonStr + ')'
 
-elif regionToPlot == 'point':
-    cntrlDsetLoiPoi = cntrlDset.sel(z_t=levOfInt, lat=latOfInt, lon=lonOfInt, method="nearest")
-    dataKey = pgf.discover_data_var(cntrlDset)
-    cntrlDarrLoiPoi = cntrlDsetLoiPoi[dataKey]
-    cntrlDarrMnSpc = cntrlDarrLoiPoi #need better variable name
+plt.figure()
+plt.plot(bndDct['mtchYrs'],cntrlToPlot.data,color='#DF8C20',label='RCP8.5') #These are the cuckooColormap colors
+plt.plot(bndDct['mtchYrs'],fdbckToPlot.data,color='#20DFCC',label='SAI')
+plt.legend()
+plt.ylabel(yStr)
+plt.title('TropCO' + ': ' + startStr + '-' + endStr + ' ' + locTitleStr)
+plt.autoscale(enable=True, axis='x', tight=True)
+# plt.title(varStr + ' ' + levStr + ': ' + startStr + '-' + endStr + ' ' + locTitleStr)
+# plt.savefig(saveName + locStr + '_' + levStr + '.png',dpi=dpi_val,bbox_inches='tight')
+plt.savefig(saveName + locStr + '_' + '.png',dpi=dpi_val,bbox_inches='tight')
 
-    fdbckDsetLoiPoi = fdbckDset.sel(z_t=levOfInt, lat=latOfInt, lon=lonOfInt, method="nearest")
-    dataKey = pgf.discover_data_var(fdbckDset)
-    fdbckDarrLoiPoi = fdbckDsetLoiPoi[dataKey]
-    fdbckDarrMnSpc = fdbckDarrLoiPoi
-
-else:
-    print("Invalid region!")
-
-
-# take 2010-2019 average and remove this
-# baselineMeanToRmv = dot.average_over_years(cntrlDarrMnSpc,2010,2019)
-# cntrlDarrMnSpcNorm = cntrlDarrMnSpc - baselineMeanToRmv
-# fdbckDarrMnSpcNorm = fdbckDarrMnSpc - baselineMeanToRmv
-
-iqr = stats.iqr(cntrlDarrMnSpc)
-binwidth = 2*iqr*(10 ** -1/3) # the Freedman-Diaconis rule
-binwidth = 0.2 #the let's not overthink this rule
-print(binwidth)
-
-cntrlActive = cntrlDarrMnSpc
-fdbckActive = fdbckDarrMnSpc
-
-cntrlYears = cntrlActive['time'].dt.year.data
-cntrlHandlesToPlot = list()
-cntrlHandlesToPlot = pgf.extract_doi(cntrlIntToPlot, cntrlYears, timePeriod, cntrlActive, cntrlHandlesToPlot)
-
-fdbckYears = fdbckActive['time'].dt.year.data
-fdbckHandlesToPlot = list()
-fdbckHandlesToPlot = pgf.extract_doi(fdbckIntToPlot, fdbckYears, timePeriod, fdbckActive, fdbckHandlesToPlot)
-
-handlesToPlot = cntrlHandlesToPlot + fdbckHandlesToPlot
-
-if baselineFlag:
-    colorsToPlot = plt_tls.select_colors(baselineFlag,len(cntrlIntToPlot)-1,len(fdbckIntToPlot))
-else:
-    colorsToPlot = plt_tls.select_colors(baselineFlag,len(cntrlIntToPlot),len(fdbckIntToPlot))
-
-if baselineFlag:
-    labelsToPlot = list(['2010-2019 Baseline'])
-else:
-    labelsToPlot = list()
-labelsToPlot = plt_tls.generate_labels(labelsToPlot, cntrlIntToPlot, timePeriod, 'RCP8.5')
-labelsToPlot = plt_tls.generate_labels(labelsToPlot, fdbckIntToPlot, timePeriod, 'SAI')
-labelsToPlot.append(titleStr)
-
-print(colorsToPlot)
-if plotStyle == 'kde':
-    plt_tls.plot_pdf_kdeplot(handlesToPlot, colorsToPlot, labelsToPlot, savePath, saveName, dpiVal)
-elif plotStyle == 'hist':
-    plt_tls.plot_pdf_hist(handlesToPlot, colorsToPlot, labelsToPlot, savePath, saveName, binwidth, dpiVal)
-else:
-    print("Invalid plot style")
-
-print('Completed!')
+print("Completed! :D")
