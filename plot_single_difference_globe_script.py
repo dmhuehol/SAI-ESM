ede3e0e1a8e3c8f0f99b90c2d0fb4896665ff733
Daniel Hueholt <dmhuehol@ncsu.edu>
2021-05-19 14:01:07 -0600

    Tidy plot_single_difference_globe_script
    Prepare for modularizing level and region blocks

--- plot_single_difference_globe_script.py
+++ plot_single_difference_globe_script.py
@@ -1,5 +1,8 @@
 # Plot GLENS data on a globe given location or region (eventually)
 
+from icecream import ic
+import sys
+
 import xarray as xr
 import matplotlib.pyplot as plt
 from matplotlib import cm
@@ -7,47 +10,63 @@ import cartopy
 import cartopy.crs as ccrs
 import cmocean
 import numpy as np
-import sys
 
 import difference_over_time as dot
 import process_glens_fun as pgf
 import plotting_tools as plt_tls
 
 # Inputs
-filenameCntrl = 'annual_control.nc'#'b.e15.B5505C5WCCML45BGCR.f09_g16.control.001.cam.h0.T.201001-201912.nc'
-filenameFdbck = 'annual_feedback.nc'#'b.e15.B5505C5WCCML45BGCR.f09_g16.feedback.001.cam.h0.T.209001-209912.nc'
-dataPath = '/Users/dhueholt/Documents/GLENS_data/attemptMap/'
+filenameCntrl = 'control_003_O3_201001-201912_202001-202912_203001-203912_204001-204912_205001-205912_206001-206912_207001-207912_208001-208912_209001-209912_annual.nc'
+filenameFdbck = 'feedback_003_O3_202001-202912_203001-203912_204001-204912_205001-205912_206001-206912_207001-207912_208001-208912_209001-209912_annual.nc'
+dataPath = '/Users/dhueholt/Documents/GLENS_data/annual_o3/'
 cntrlPath = dataPath + filenameCntrl
 fdbckPath = dataPath + filenameFdbck
 
-savePath = '/Users/dhueholt/Documents/GLENS_fig/20210401_dataformat/'
-dpi_val = 400
-
-startInt = [2010,2019]
+startInt = [2020,2029]
 finalInt = [2090,2099]
-levOfInt = 1000 #hPa
+# levOfInt = 992.6 #hPa
 quantileForFig = 0.66
 regionToPlot = 'global' #aspirational
 
-# Control
+savePath = '/Users/dhueholt/Documents/GLENS_fig/20210519_regionsAndOzone/'
+savePrfx = 'INPROGRESS_globe_1p_FdbckCntrl_'
+dpi_val = 400
+
+# Open data
 glensDsetCntrl = xr.open_dataset(cntrlPath)
+glensDsetFdbck = xr.open_dataset(fdbckPath)
 dataKey = pgf.discover_data_var(glensDsetCntrl)
-glensDarrCntrl = glensDsetCntrl[dataKey] #HERE IT IS
-print(glensDarrCntrl)
+glensDarrCntrl = glensDsetCntrl[dataKey]
+glensDarrFdbck = glensDsetFdbck[dataKey]
+
+# Level (TODO: REPLACE)
+levOfInt = glensDarrCntrl['lev'].data[len(glensDarrCntrl['lev'].data)-1-16]
+levs = glensDarrCntrl['lev'].data
+levMask = levs > levOfInt
+
+ic(glensDarrCntrl)
+# Average over years but also apply level mask (TODO: REFACTOR)
 toiStart = dot.average_over_years(glensDarrCntrl,startInt[0],startInt[1]) # 2010-2019 is baseline, injection begins 2020
 toiEndCntrl = dot.average_over_years(glensDarrCntrl,finalInt[0],finalInt[1])
+toiEndCntrl = toiEndCntrl[levMask,:,:]
+# toiEndCntrl = toiEndCntrl.sel(lat=latOfInt, lon=lonOfInt, method="nearest")
+toiEndCntrl = toiEndCntrl.sum(dim='lev')
 # diffToiCntrl = toiEndCntrl - toiStart
 #format: lev,lat,lon
 
-# Feedback
-glensDsetFdbck = xr.open_dataset(fdbckPath)
-glensDarrFdbck = glensDsetFdbck[dataKey]
 toiEndFdbck = dot.average_over_years(glensDarrFdbck,finalInt[0],finalInt[1])
+toiEndFdbck = toiEndFdbck[levMask,:,:]
+# toiEndFdbck = toiEndFdbck.sel(lat=latOfInt, lon=lonOfInt, method="nearest")
+toiEndFdbck = toiEndFdbck.sum(dim='lev')
 # diffToiFdbck = toiEndFdbck - toiStart
-diffToiFdbck = toiEndFdbck - toiEndCntrl
+diffToiFdbck =  toiEndFdbck - toiEndCntrl
 
 # Calculate
-diffToiFdbck = diffToiFdbck.sel(lev=levOfInt)
+# print(diffToiFdbck['lev'][0].data)
+# diffToiFdbck = diffToiFdbck.sum(dim='lev')#
+
+# diffToiFdbck = diffToiFdbck.sel(lev=diffToiFdbck['lev'][0].data)
+
 # diffToiLevCntrl = diffToiCntrl.sel(lev=levOfInt)
 # diffToiLevFdbck = diffToiFdbck.sel(lev=levOfInt)
 
@@ -74,24 +93,25 @@ firstDcd = str(startInt[0]) + '-' + str(startInt[1])
 lastDcd = str(finalInt[0]) + '-' + str(finalInt[1])
 cntrlStr = 'RCP8.5'
 # fdbckStr = 'SAI'
-tempStr = 'SAI - RCP8.5'
-levStr = str(levOfInt) + 'mb'
+tempStr = 'RCP8.5 - SAI'
+levStr = "TropCO" #str(levOfInt) + 'mb'
 varStr = glensDarrCntrl.long_name
 quantileStr = str(quantileForFig)
 
 plt.figure(figsize=(12,2.73*2))
 ax = plt.subplot(1,1,1,projection=mapProj) #nrow ncol index
-cmap = cmocean.cm.haline
+cmap = cmocean.cm.delta#cmocean.cm.haline
 cmapSeq = cm.Purples
-minVal = -8#-diffToiFdbck.quantile(0.97).data
-maxVal = 1#diffToiFdbck.quantile(0.97).data
+minVal = -diffToiFdbck.quantile(0.99).data
+maxVal = diffToiFdbck.quantile(0.99).data
 
+ic(diffToiFdbck)
 plt_tls.drawOnGlobe(ax, diffToiFdbck, glensDarrFdbck.lat, glensDarrFdbck.lon, cmap, vmin=minVal, vmax=maxVal, cbarBool=True, fastBool=True, extent='max')
 # plt.title(lastDcd + ' - ' + firstDcd + ' ' + tempStr + ' ' + levStr + ' ' + varStr)
 plt.title(lastDcd + ' ' + tempStr + ' ' + levStr + ' ' + varStr)
 
-prfxStr = '1p_FdbckCntrl_'
-saveStr = prfxStr + dataKey + str(levOfInt) + '_' + regionToPlot + '_' + str(startInt[0]) + str(startInt[1]) + '_' + str(finalInt[0]) + str(finalInt[1])
+saveStr = savePrfx + dataKey + "TropCO" + '_' + regionToPlot + '_' + str(startInt[0]) + str(startInt[1]) + '_' + str(finalInt[0]) + str(finalInt[1])
+# saveStr = savePrfx + dataKey + str(levOfInt) + '_' + regionToPlot + '_' + str(startInt[0]) + str(startInt[1]) + '_' + str(finalInt[0]) + str(finalInt[1])
 savename = savePath + saveStr + '.png'
 plt.savefig(savename,dpi=dpi_val,bbox_inches='tight')
 
