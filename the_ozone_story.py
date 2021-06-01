2c77f24ff0a5840093ec511dbb6c4a3821646c30
Daniel Hueholt <dmhuehol@ncsu.edu>
2021-06-01 16:41:39 -0600

    Refactor the_ozone_story
    Tidying, especially making things more self-contained

--- the_ozone_story.py
+++ the_ozone_story.py
@@ -23,13 +23,8 @@ FiraSansMed = fm.FontProperties(fname=fontPath+firaSansPath['Medium'])
 FiraSansThin = fm.FontProperties(fname=fontPath+firaSansPath['Thin'])
 import matplotlib.pyplot as plt
 from matplotlib import cm
-# ic(plt.rcParams['font.sans-serif'])
-# plt.rcParams['font.family'] = 'sans-serif'
-# plt.rcParams['font.sans-serif'] = 'Fira Sans Thin'
 import cartopy
 import cartopy.crs as ccrs
-import cmocean
-import cmasher
 import numpy as np
 
 import difference_over_time as dot
@@ -43,21 +38,24 @@ filenameCntrl = 'control_003_O3_201001-201912_202001-202912_203001-203912_204001
 filenameFdbck = 'feedback_003_O3_202001-202912_203001-203912_204001-204912_205001-205912_206001-206912_207001-207912_208001-208912_209001-209912_annual.nc'
 cntrlPath = dataPath + filenameCntrl
 fdbckPath = dataPath + filenameFdbck
-#
+
 startInt = [2010,2019]
 finalInt = [2090,2099]
 
-def to3s_strat():
-    levOfInt = 'stratosphere'
-    savePath = '/Users/dhueholt/Documents/GLENS_fig/20210528_theOzoneStory/'
-    savePrfx = 'NORM_globe_1p_FdbckCntrl_'
-    dpi_val = 800
-    # Open data
-    glensDsetCntrl = xr.open_dataset(cntrlPath)
-    glensDsetFdbck = xr.open_dataset(fdbckPath)
-    dataKey = pgf.discover_data_var(glensDsetCntrl)
-    glensDarrCntrl = glensDsetCntrl[dataKey]
-    glensDarrFdbck = glensDsetFdbck[dataKey]
+# Open data
+glensDsetCntrl = xr.open_dataset(cntrlPath)
+glensDsetFdbck = xr.open_dataset(fdbckPath)
+dataKey = pgf.discover_data_var(glensDsetCntrl)
+glensDarrCntrl = glensDsetCntrl[dataKey]
+glensDarrFdbck = glensDsetFdbck[dataKey]
+
+savePath='/Users/dhueholt/Documents/GLENS_fig/20210601_theOzoneStory/'
+dpi_val = 800
+
+def to3s_dg(levOfInt,tStr):
+    ''' Makes difference globe '''
+
+    savePrfx='NORM_globe_1p_FdbckCntrl_'
 
     # Obtain levels
     glensCntrlLoi = pgf.obtain_levels(glensDarrCntrl, levOfInt)
@@ -65,7 +63,6 @@ def to3s_strat():
 
     # Average over years
     toiStart = dot.average_over_years(glensCntrlLoi, startInt[0], startInt[1]) # 2010-2019 is baseline, injection begins 2020
-    # toiEndCntrl = dot.average_over_years(glensCntrlLoi, finalInt[0], finalInt[1])
     toiEndFdbck = dot.average_over_years(glensFdbckLoi, finalInt[0], finalInt[1])
     diffToiFdbck =  toiEndFdbck - toiStart
 
@@ -75,100 +72,33 @@ def to3s_strat():
     diffToiFdbckPlotNorm = diffToiFdbckPlot / np.max(toiStartUnit) * 100
 
     # Plotting
-    firstDcd = str(startInt[0]) + '-' + str(startInt[1])
     lastDcd = str(finalInt[0]) + '-' + str(finalInt[1])
-    sceneStr = 'RCP8.5 - SAI'
     levStr = pgf.make_level_string(glensCntrlLoi, levOfInt)
-    varStr = glensDarrCntrl.long_name
 
     CL = 0.
     mapProj = cartopy.crs.EqualEarth(central_longitude = CL)
     plt.figure(figsize=(12, 2.73*2))
     ax = plt.subplot(1, 1, 1, projection=mapProj) #nrow ncol index
-    cmap = 'PuOr'#cmasher.prinsenvlag #cmocean.cm.delta
-    # minVal = -diffToiFdbckPlot.quantile(0.99).data
-    minVal = -15 #Override automatic colorbar minimum here
-    # maxVal = diffToiFdbckPlot.quantile(0.99).data
-    maxVal = 15 #Override automatic colorbar maximum here
+    cmap = 'PuOr'
+    minVal = -15
+    maxVal = 15
 
     plt_tls.drawOnGlobe(ax, diffToiFdbckPlotNorm, glensDarrFdbck.lat, glensDarrFdbck.lon, cmap, vmin=minVal, vmax=maxVal, cbarBool=True, fastBool=True, extent='max')
-    # plt.title(lastDcd + ' ' + sceneStr + ' ' + levStr + ' ' + varStr)
-    plt.title("Percent change in 250mb to 50mb ozone 2010-2019 to 2090-2099",fontproperties=FiraSansMed,fontsize=18) #Override automatic title generation here
+    plt.title(tStr,fontproperties=FiraSansMed,fontsize=18) #Override automatic title generation here
     saveStr = savePrfx + dataKey + '_' + levStr + '_' + lastDcd
-    # saveStr = 'globe_1p_FdbckCntrl_O3_[50 0]_C2010-2019_F2090-2099'#Override automatic filename generation here
     savename = savePath + saveStr + '.png'
     plt.savefig(savename, dpi=dpi_val, bbox_inches='tight')
     ic(savename)
 
-def to3s_25050():
-    levOfInt = [250,50] #'troposphere', 'total', numeric level, or list of numeric levels
-    savePath = '/Users/dhueholt/Documents/GLENS_fig/20210528_theOzoneStory/'
-    savePrfx = 'NORM_globe_1p_FdbckCntrl_'
-    dpi_val = 800
-    # Open data
-    glensDsetCntrl = xr.open_dataset(cntrlPath)
-    glensDsetFdbck = xr.open_dataset(fdbckPath)
-    dataKey = pgf.discover_data_var(glensDsetCntrl)
-    glensDarrCntrl = glensDsetCntrl[dataKey]
-    glensDarrFdbck = glensDsetFdbck[dataKey]
-
-    # Obtain levels
-    glensCntrlLoi = pgf.obtain_levels(glensDarrCntrl, levOfInt)
-    glensFdbckLoi = pgf.obtain_levels(glensDarrFdbck, levOfInt)
-
-    # Average over years
-    toiStart = dot.average_over_years(glensCntrlLoi, startInt[0], startInt[1]) # 2010-2019 is baseline, injection begins 2020
-    # toiEndCntrl = dot.average_over_years(glensCntrlLoi, finalInt[0], finalInt[1])
-    toiEndFdbck = dot.average_over_years(glensFdbckLoi, finalInt[0], finalInt[1])
-    diffToiFdbck =  toiEndFdbck - toiStart
-
-    # Unit conversion
-    diffToiFdbckPlot = fcu.molmol_to_ppm(diffToiFdbck)
-    toiStartUnit = fcu.molmol_to_ppm(toiStart)
-    diffToiFdbckPlotNorm = diffToiFdbckPlot / np.max(toiStartUnit) * 100
-
-    # Plotting
-    firstDcd = str(startInt[0]) + '-' + str(startInt[1])
-    lastDcd = str(finalInt[0]) + '-' + str(finalInt[1])
-    sceneStr = 'RCP8.5 - SAI'
-    levStr = pgf.make_level_string(glensCntrlLoi, levOfInt)
-    varStr = glensDarrCntrl.long_name
-
-    CL = 0.
-    mapProj = cartopy.crs.EqualEarth(central_longitude = CL)
-    plt.figure(figsize=(12, 2.73*2))
-    ax = plt.subplot(1, 1, 1, projection=mapProj) #nrow ncol index
-    cmap = 'PuOr'#cmasher.prinsenvlag #cmocean.cm.delta
-    # minVal = -diffToiFdbckPlot.quantile(0.99).data
-    minVal = -15 #Override automatic colorbar minimum here
-    # maxVal = diffToiFdbckPlot.quantile(0.99).data
-    maxVal = 15 #Override automatic colorbar maximum here
-
-    plt_tls.drawOnGlobe(ax, diffToiFdbckPlotNorm, glensDarrFdbck.lat, glensDarrFdbck.lon, cmap, vmin=minVal, vmax=maxVal, cbarBool=True, fastBool=True, extent='max')
-    # plt.title(lastDcd + ' ' + sceneStr + ' ' + levStr + ' ' + varStr)
-    plt.title("Percent change in 250mb to 50mb ozone 2010-2019 to 2090-2099",fontproperties=FiraSansMed,fontsize=18) #Override automatic title generation here
-    saveStr = savePrfx + dataKey + '_' + levStr + '_' + lastDcd
-    # saveStr = 'globe_1p_FdbckCntrl_O3_[50 0]_C2010-2019_F2090-2099'#Override automatic filename generation here
-    savename = savePath + saveStr + '.png'
-    plt.savefig(savename, dpi=dpi_val, bbox_inches='tight')
-    ic(savename)
+to3s_dg(levOfInt='stratosphere',tStr="Percent change in stratospheric ozone 2010-2019 to 2090-2099")
+to3s_dg(levOfInt=[250,50],tStr="Percent change in 250mb to 50mb ozone 2010-2019 to 2090-2099")
 
 def to3s_ts():
-    levOfInt = 'stratosphere' #'stratosphere', 'troposphere', 'total', numeric level, or list of numeric levels
-    regionToPlot = 'global' #'global', rlib.Place(), [latN,lonE360]
-
-    savePath = '/Users/dhueholt/Documents/GLENS_fig/20210528_theOzoneStory/'
+    ''' Make stratospheric ozone timeseries '''
+    levOfInt = 'stratosphere'
+    regionToPlot = 'global'
     saveFile = 'timeseries_O3_'
     saveName = savePath + saveFile
-    dpi_val = 400
-
-    # Open data
-    glensDsetCntrl = xr.open_dataset(cntrlPath)
-    glensDsetFdbck = xr.open_dataset(fdbckPath)
-    dataKey = pgf.discover_data_var(glensDsetCntrl)
-    #dataKey = '' #Override automatic variable discovery here
-    glensDarrCntrl = glensDsetCntrl[dataKey]
-    glensDarrFdbck = glensDsetFdbck[dataKey]
 
     bndDct = pgf.find_matching_year_bounds(glensDarrCntrl, glensDarrFdbck)
     glensCntrlPoi = glensDarrCntrl[bndDct['cntrlStrtMtch']:bndDct['cntrlEndMtch']+1] #RANGES IN PYTHON ARE [)
@@ -188,30 +118,24 @@ def to3s_ts():
 
     # Plotting
     yStr = 'parts per million'
-    varStr = glensDarrFdbck.long_name
-    startStr = str(bndDct['strtYrMtch'])
-    endStr = str(bndDct['endYrMtch'])
     levStr = pgf.make_level_string(glensCntrlPoi, levOfInt)
 
     # Make timeseries
     fig, ax = plt.subplots()
     plt.plot(bndDct['mtchYrs'],cntrlToPlot.data,color='#DF8C20',label='RCP8.5') #These are the cuckooColormap colors
     plt.plot(bndDct['mtchYrs'],fdbckToPlot.data,color='#20DFCC',label='SAI')
-    ic()
     ax.annotate('Global stratospheric ozone concentration',(0.02,0.935),(0.02,0.935),'axes fraction',fontproperties=FiraSansMed,fontsize=11,color='#000000')
     ax.annotate('RCP8.5',(0.236,0.482),(0.236,0.482),'axes fraction',fontproperties=FiraSansMed,fontsize=11,color='#DF8C20')
     ax.annotate('SAI',(0.419,0.28),(0.419,0.28),'axes fraction',fontproperties=FiraSansMed,fontsize=11,color='#20DFCC')
-    # plt.text(40,131,'RCP8.5',fontproperties=FiraSansMed,fontsize=14,color='#DF8C20')
     plt.ylabel(yStr,fontproperties=FiraSansThin)
-    # plt.xlabel('Time',fontproperties=FiraSansThin)
     for label in ax.get_xticklabels():
         label.set_fontproperties(FiraSansThin)
     for label in ax.get_yticklabels():
         label.set_fontproperties(FiraSansThin)
     plt.autoscale(enable=True, axis='x', tight=True)
-    # plt.title(varStr + ' ' + levStr + ': ' + startStr + '-' + endStr + ' ' + locTitleStr,fontproperties=FiraSansMed,fontsize=18)
-    # plt.title('Global stratospheric ozone concentration',fontproperties=FiraSansMed,fontsize=18)
     plt.savefig(saveName + locStr + '_' + levStr + '.png',dpi=dpi_val,bbox_inches='tight')
     ic(saveName + locStr + '_' + levStr + '.png')
 
     print('Completed!')
+
+to3s_ts()
