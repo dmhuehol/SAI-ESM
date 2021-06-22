5c33fdc3f052442b50095039f9049b62e1bd5eb0
Daniel Hueholt <dmhuehol@ncsu.edu>
2021-06-22 12:55:30 -0600

    Add IPCC WG1-AR5 boxes
    Adds rectangle-shaped regions from the IPCC WG1-AR5 atlas.  wrap_test_script has code to make test figures

--- region_library.py
+++ region_library.py
@@ -14,6 +14,207 @@ import matplotlib.pyplot as plt
 
 import plotting_tools as plt_tls
 
+### IPCC regions used in WG1-AR5
+
+def AlaskaNorthwestCanada():
+    regDict = {
+        "regStr": 'Alaska/Northwest Canada',
+        "regSaveStr": 'AKNWCan',
+        "regLats": np.array([60, 72.6]),
+        "regLons": np.array([192, 255])
+    }
+
+    return regDict
+
+def CentralAsia():
+    regDict = {
+        "regStr": 'Central Asia',
+        "regSaveStr": 'CentrlAsia',
+        "regLats": np.array([30, 50]),
+        "regLons": np.array([60, 75])
+    }
+
+    return regDict
+
+def CanadaGreenlandIceland():
+    regDict = {
+        "regStr": 'Canada/Greenland/Iceland',
+        "regSaveStr": 'CanGrnIce',
+        "regLats": np.array([50, 85]),
+        "regLons": np.array([255, 350])
+    }
+
+    return regDict
+
+def EastAfrica():
+    regDict = {
+        "regStr": 'East Africa',
+        "regSaveStr": 'EastAfrica',
+        "regLats": np.array([-11.4, 15]),
+        "regLons": np.array([25, 52])
+    }
+
+    return regDict
+
+def EastAsia():
+    regDict = {
+        "regStr": 'East Asia',
+        "regSaveStr": 'EastAsia',
+        "regLats": np.array([20, 50]),
+        "regLons": np.array([100, 145])
+    }
+
+    return regDict
+
+def EastNorthAmerica():
+    regDict = {
+        "regStr": 'East N America',
+        "regSaveStr": 'EstNAm',
+        "regLats": np.array([25, 50]),
+        "regLons": np.array([275, 300])
+    }
+
+    return regDict
+
+def NorthAsia():
+    regDict = {
+        "regStr": 'North Asia',
+        "regSaveStr": 'NrthAsia',
+        "regLats": np.array([50, 70]),
+        "regLons": np.array([40, 180])
+    }
+
+    return regDict
+
+def NorthAustralia():
+    regDict = {
+        "regStr": 'North Australia',
+        "regSaveStr": 'NrthAustrla',
+        "regLats": np.array([-30, -10]),
+        "regLons": np.array([110, 155])
+    }
+
+    return regDict
+
+def NortheastBrazil():
+    regDict = {
+        "regStr": 'Northeast Brazil',
+        "regSaveStr": 'NrthestBrazil',
+        "regLats": np.array([-20, 0]),
+        "regLons": np.array([310, 326])
+    }
+
+    return regDict
+
+def SouthAustraliaNewZealand():
+    regDict = {
+        "regStr": 'South Australia/New Zealand',
+        "regSaveStr": 'SAusNewZlnd',
+        "regLats": np.array([-50, -30]),
+        "regLons": np.array([110, 180])
+    }
+
+    return regDict
+
+def SoutheastAsia():
+    regDict = {
+        "regStr": 'Southeast Asia',
+        "regSaveStr": 'SEAsia',
+        "regLats": np.array([-10, 20]),
+        "regLons": np.array([95, 155])
+    }
+
+    return regDict
+
+def TibetanPlateau():
+    regDict = {
+        "regStr": 'Tibetan Plateau',
+        "regSaveStr": 'TibetPlat',
+        "regLats": np.array([30, 50]),
+        "regLons": np.array([75, 100])
+    }
+
+    return regDict
+
+def WestAsia():
+    regDict = {
+        "regStr": 'West Asia',
+        "regSaveStr": 'WestAsia',
+        "regLats": np.array([15, 50]),
+        "regLons": np.array([40, 60])
+    }
+
+    return regDict
+
+def WestNorthAmerica():
+    regDict = {
+        "regStr": 'West N America',
+        "regSaveStr": 'WstNAm',
+        "regLats": np.array([28.6, 60]),
+        "regLons": np.array([230, 255])
+    }
+
+    return regDict
+
+def Antarctica():
+    regDict = {
+        "regStr": 'Antarctica',
+        "regSaveStr": 'Antarctica',
+        "regLats": np.array([-90, -50]),
+        "regLons": np.array([0, 360])
+    }
+
+    return regDict
+
+def Arctic():
+    regDict = {
+        "regStr": 'Arctic',
+        "regSaveStr": 'Arctic',
+        "regLats": np.array([67.5, 90]),
+        "regLons": np.array([0, 360])
+    }
+
+    return regDict
+
+def PacificIslandsRegion2():
+    regDict = {
+        "regStr": 'Pacific Islands Region[2]',
+        "regSaveStr": 'PacIslReg2',
+        "regLats": np.array([5, 25]),
+        "regLons": np.array([155, 210])
+    }
+
+    return regDict
+
+def PacificIslandsRegion3():
+    regDict = {
+        "regStr": 'Pacific Islands Region[3]',
+        "regSaveStr": 'PacIslReg3',
+        "regLats": np.array([-5, 5]),
+        "regLons": np.array([155, 230])
+    }
+
+    return regDict
+
+def SouthernTropicalPacific():
+    regDict = {
+        "regStr": 'Southern Tropical Pacific',
+        "regSaveStr": 'STropPac',
+        "regLats": np.array([-25,-5]),
+        "regLons": np.array([155, 230])
+    }
+
+    return regDict
+
+def WestIndianOcean():
+    regDict = {
+        "regStr": 'West Indian Ocean',
+        "regSaveStr": 'WIndOcn',
+        "regLats": np.array([-25, 5]),
+        "regLons": np.array([52, 75])
+    }
+
+    return regDict
 
 ### Oceans
 
@@ -213,4 +414,5 @@ def test_region(region):
     ax = plt.subplot(1, 1, 1, projection=mapProj) #nrow ncol index
     plt_tls.drawOnGlobe(ax, plotOnes, lats, lons, cmap='viridis', vmin=0, vmax=2, cbarBool=True, fastBool=True, extent='max')
     plt.title(region["regStr"])
-    plt.show()
+    # plt.show()
+    plt.savefig('/Users/dhueholt/Documents/GLENS_fig/20210622_ipccRegions/' + region["regSaveStr"] + '.png', dpi=400)
