43301ed058c68f40f5a6d43b4941b3683889ab2e
Daniel Hueholt <dmhuehol@ncsu.edu>
2021-10-26 10:55:59 -0600

    Derive data for multiple scenarios in single run
    This uses the same method as the cdo processing does to loop through scenarios in a single run. Note that this requires the variable key to be the same in each file.

--- wrap_derive_data_script.py
+++ wrap_derive_data_script.py
@@ -19,20 +19,21 @@ import subprocess
 import fun_derive_data as fdd
 
 inPath = '/glade/scratch/dhueholt/daily_TREFHTMN/mergetime/'#'/Users/dhueholt/Documents/GLENS_data/daily_TREFHT/mergetime/'
-inToken = '*control*'#'*control*','*feedback*','*SSP245-TSMLT*','*BWSSP245*','*BWHIST*' #tokens for GLENS, ARISE, CESM2-WACCM, historical #use ONE AT A TIME for now
+inToken = ['*control*','*feedback*','*SSP245-TSMLT*','*BWSSP245*','*BWHIST*']#'*control*','*feedback*','*SSP245-TSMLT*','*BWSSP245*','*BWHIST*' #tokens for GLENS, ARISE, CESM2-WACCM, historical #use ONE AT A TIME for now
 outPath = '/glade/scratch/dhueholt/clxTR/'#'/Users/dhueholt/Documents/GLENS_data/clxTR/'
-nProc = 10
+nProc = 13
 
-theGlob = glob.glob(inPath+inToken)
-for fc,fn in enumerate(theGlob):
-    ic(fn)
-    if __name__== '__main__': #If statement required by multiprocessing
-        shard = Process(target=fdd.derive_annual_tropical_nights, args=(fn,outPath))
-    if fc % nProc == 0 and fc != 0:
-        shard.start()
-        shard.join() #Forces nProc+1 processes to run to completion before beginning more
-        shard.close() #Free up all associated resources
-    else:
-        shard.start()
-    filesRemaining = len(theGlob) - fc - 1
-    ic(filesRemaining) #Cheap "progress bar"
+for scen in inToken:
+    theGlob = glob.glob(inPath+scen)
+    for fc,fn in enumerate(theGlob):
+        ic(fn)
+        if __name__== '__main__': #If statement required by multiprocessing
+            shard = Process(target=fdd.derive_annual_tropical_nights, args=(fn,outPath))
+        if fc % nProc == 0 and fc != 0:
+            shard.start()
+            shard.join() #Forces nProc+1 processes to run to completion before beginning more
+            shard.close() #Free up all associated resources
+        else:
+            shard.start()
+        filesRemaining = len(theGlob) - fc - 1
+        ic(filesRemaining) #Cheap "progress bar"
