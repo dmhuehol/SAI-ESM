1adacca66ca68dc59693516170bb17eb38b79296
Daniel Hueholt <dmhuehol@ncsu.edu>
2022-01-11 16:01:26 -0700

    Document get bees and simplify logging

--- get_data/get_bees/do_acntrl_copytolocal.sh
+++ get_data/get_bees/do_acntrl_copytolocal.sh
@@ -1,23 +1,17 @@
-#!/bin/bash -l
-### Job Name
-#PBS -N copy_acntrl_var
-### Project code
-#PBS -A P06010014
-#PBS -l walltime=10:00
-#PBS -q casper
-### Merge output and error files
-#PBS -j oe
-### Select 1 nodes with 1 CPUs each
-#PBS -l select=1:ncpus=1:mem=10GB
-### Send email on abort, begin and end
-#PBS -m abe
-### Specify mail recipient
-#PBS -M dhueholt@rams.colostate.edu
-exec &> logfile_get_arisecntrl.txt
+#######################################
+# Copy ARISE control files to local directory.
+# Globals:
+#   None
+# Arguments:
+#   IN_PATH
+#   MOD_TOKEN
+#   TIME_TOKEN
+#   OUT_PATH
+# Written by Daniel Hueholt
+# Graduate Research Assistant at Colorado State University
+#######################################
 
-export TMPDIR=/glade/scratch/dhueholt/temp
-mkdir -p $TMPDIR
-
+### Input variables
 IN_TOKEN=$1
 MOD_TOKEN=$2
 TIME_TOKEN=$3
