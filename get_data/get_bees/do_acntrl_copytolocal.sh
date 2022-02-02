38a2cbf744ea8a32ef0f7a1c6fd4d019762b04f7
Daniel Hueholt <dmhuehol@ncsu.edu>
2022-02-01 20:51:53 -0700

    Update ARISE-SAI directory
    ARISE-SAI is now located at its final directory

--- get_data/get_bees/do_acntrl_copytolocal.sh
+++ get_data/get_bees/do_acntrl_copytolocal.sh
@@ -36,18 +36,18 @@ for emrf in ${EMEMRF[@]}; do
 done
 
 ### Raw ARISE dedicated futures
-# CMN_PATHAF="/glade/campaign/cesm/development/wawg/WACCM6-TSMLT-SSP245/b.e21.BWSSP245cmip6.f09_g17.CMIP6-SSP2-4.5-WACCM."
-# EMEMAF=(
-# "006"
-# "007"
-# "008"
-# "009"
-# "010"
-# )
-# # CMN_PATHAF + EMEMAF + MOD_TOKEN + PROC + TIME_TOKEN = directory structure for each ens member
-#
-# for emaf in ${EMEMAF[@]}; do
-#     FILE_TO_COPYAF=$CMN_PATHAF$emaf$S$MOD_TOKEN$PROC$TIME_TOKEN$IN_TOKEN
-#     echo $FILE_TO_COPYAF
-#     # cp $FILE_TO_COPYAF $OUT_PATH
-# done
+CMN_PATHAF="/glade/campaign/cesm/collections/CESM2-WACCM-SSP245/b.e21.BWSSP245cmip6.f09_g17.CMIP6-SSP2-4.5-WACCM."
+EMEMAF=(
+"006"
+"007"
+"008"
+"009"
+"010"
+)
+# CMN_PATHAF + EMEMAF + MOD_TOKEN + PROC + TIME_TOKEN = directory structure for each ens member
+
+for emaf in ${EMEMAF[@]}; do
+    FILE_TO_COPYAF=$CMN_PATHAF$emaf$S$MOD_TOKEN$PROC$TIME_TOKEN$IN_TOKEN
+    # echo $FILE_TO_COPYAF
+    cp $FILE_TO_COPYAF $OUT_PATH
+done
