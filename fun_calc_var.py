Fix climate velocity and dcd calculation
Fix incorrect dimension, Sobel call, and order of operations so all calculations are now done before ensemble mean

fun_calc_var.py
@@ -20,7 +20,7 @@ import xarray as xr
 def calc_temporal_grad(darr, years):
     ''' Calculate temporal gradient by estimating 'heights' between a start and
     end time. Vectorized linear regression is strongly based on
-    hrishichandanpurkar.blogspot.com/2017/0 9/vectorized-functions-for-correlation.html
+    hrishichandanpurkar.blogspot.com/2017/09/vectorized-functions-for-correlation.html
     implementation '''
     startTime = cftime.DatetimeNoLeap(years[0], 7, 15, 12, 0, 0, 0)
     endTime = cftime.DatetimeNoLeap(years[1], 7, 15, 12, 0, 0, 0)
@@ -33,7 +33,12 @@ def calc_temporal_grad(darr, years):
     xTimeStdev  = xData.std(dim='time')
     yTimeStdev  = darr.std(dim='time')
     # ic(timeEntries, xTimeMn, yTimeMn, xTimeStdev, yTimeStdev)
+    # ic(np.shape(timeEntries), np.shape(xTimeMn), np.shape(yTimeMn), np.shape(xTimeStdev), np.shape(yTimeStdev))
+    # ic(np.shape((xData - xTimeMn)*(darr - yTimeMn)))
+    # sys.exit('STOP')
     cov = np.sum((xData - xTimeMn)*(darr - yTimeMn), axis=0)/(timeEntries)
+    # ic(np.shape(cov))
+    # sys.exit('STOP')
     regSlp = cov/(xTimeStdev**2)
     regInt = yTimeMn - xTimeMn*regSlp
     # ic(cov, regSlp, regInt)
@@ -58,11 +63,22 @@ def calc_spatial_grad(darr):
     distY = dLat[0] * earthRad * np.pi / 180
     distX = (dLon[1]/180) * np.pi * earthRad * np.cos(latGridRad)
 
-    # Calculate north/south spatial gradient
-    nsGrad = sndimg.sobel(darr, axis=0, mode='reflect')
+    if 'realization' in darr.dims:
+        nsGradRlzList = list()
+        ewGradRlzList = list()
+        for r in darr.realization.data:
+            nsGradRlz = sndimg.sobel( # Calculate north/south spatial gradient
+                darr.isel(realization=r), axis=0, mode='reflect')
+            nsGradRlzList.append(nsGradRlz)
+            ewGradRlz = sndimg.sobel( # Calculate east/west spatial gradient
+                darr.isel(realization=r), axis=1, mode='reflect')
+            ewGradRlzList.append(ewGradRlz)
+        nsGrad = np.mean(nsGradRlzList, axis=0)
+        ewGrad = np.mean(ewGradRlzList, axis=0)
+    else:
+        nsGrad = sndimg.sobel(darr, axis=0, mode='reflect')
+        ewGrad = sndimg.sobel(darr, axis=1, mode='reflect')
     nsGradSc = nsGrad / (8 * distY)
-    # Calculate east/west spatial gradient
-    ewGrad = sndimg.sobel(darr, axis=1, mode='reflect')
     ewGradSc = ewGrad / (8 * distX)
     # Total spatial gradient
     totGrad = np.sqrt((nsGradSc ** 2) + (ewGradSc ** 2))
@@ -91,8 +107,12 @@ def calc_climate_speed(darr, setDict):
 
     tGradDict = calc_temporal_grad(darrToi, years=setYrs)
     tGrad = tGradDict["grad"].compute()
+    # ic(check_stats(np.abs(tGrad.mean(dim='realization').data)))
+    # ic(check_stats(np.abs(tGrad.data)))
+    # sys.exit('STOP')
     darrTimeMn = darrToi.mean(dim='time')
     sGrad = calc_spatial_grad(darrTimeMn)
+    # ic(check_stats(np.abs(sGrad.data)))
 
     climSpd = tGrad / sGrad
     climSpd.attrs = darr.attrs
@@ -100,9 +120,9 @@ def calc_climate_speed(darr, setDict):
     climSpd.attrs['long_name'] = 'Climate speed of 2m temperature' \
         + ' ' + str(setYrs[0]) + '-' + str(setYrs[1])
     climSpd.attrs['units'] = 'deg C / km'
-    ic(check_stats(tGrad))
-    ic(check_stats(sGrad))
-    ic(check_stats(climSpd))
+    # ic(check_stats(tGrad.data))
+    # ic(check_stats(sGrad.data))
+    # ic(check_stats(climSpd.data))
     return climSpd
 
 def calc_decadal_climate_distance(darr, setDict):
@@ -124,10 +144,10 @@ def check_stats(darr):
 
     statDict = {
         "shape": np.shape(darr),
-        "med": np.median(darr),
-        "mean": np.mean(darr),
-        "max": np.max(darr),
-        "min": np.min(darr)
+        "med": np.nanmedian(darr),
+        "mean": np.nanmean(darr),
+        "max": np.nanmax(darr),
+        "min": np.nanmin(darr)
     }
 
     return statDict

fun_plot_slice_globe.py
@@ -56,16 +56,14 @@ def plot_single_slice_globe(rlzList, dataDict, setDict, outDict):
         sys.exit('Check plotPanel input')
 
     # Obtain ens mean, pointwise max/min, or individual realization
-    if setDict["plotEnsType"] == 'mean':
-        actData = actData.isel(realization=[0,1,2,3,4])
+    if setDict["plotEnsType"] == 'mean': #Ensemble mean
         panel = actData.mean(dim='realization')
-    elif setDict["plotEnsType"] == 'max':
+    elif setDict["plotEnsType"] == 'max': #Pointwise max
         panel = actData.max(dim='realization')
-    elif setDict["plotEnsType"] == 'min':
+    elif setDict["plotEnsType"] == 'min': #Pointwise min
         panel = actData.min(dim='realization')
-    elif setDict["plotEnsType"] == 'single':
-        panel = actData
-        # panel = actData.isel(realization=setDict["plotEnsType"])
+    elif isinstance(setDict["plotEnsType"], int): #Single member
+        panel = actData.isel(realization=setDict["plotEnsType"])
     else:
         sys.exit('Check plotEnsType input!')
         

fun_process_data.py
@@ -62,7 +62,7 @@ def call_to_open(dataDict, setDict):
     for ec in lf['globs']:
         lf['emem'].append(get_ens_mem(ec))
     lf['emem'] = list(filter(None,lf['emem']))
-
+    
     for dc,darr in enumerate(lf['darr']):
         scnArr, ememStr = manage_realizations(setDict, darr, lf['emem'][dc])
         lf['scn'].append(scnArr)
@@ -75,6 +75,7 @@ def call_to_open(dataDict, setDict):
     if setDict["convert"] is not None:
         for cnvrtr in setDict["convert"]:
             for rc,rv in enumerate(lf['scn']):
+                ic(rc, rv)
                 try:
                     lf['scn'][rc] = cnvrtr(rv) #Use input converter function(s)
                 except:
@@ -216,6 +217,9 @@ def manage_realizations(setDict, darr, emem):
             else:
                 darrMn = darr.mean(dim='realization')
                 darrOut = xr.concat([darr,darrMn], dim='realization').compute() #Add ens mean as another "realization"
+            ememSave = 'ens' + scnStr
+        elif setDict['realization'] == 'allplot': #TODO: this should be only behavior
+            darrOut = darr.compute()
             ememSave = 'ens' + scnStr
         else: #Output DataArray of single ens member
             try: