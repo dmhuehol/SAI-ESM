Fix spatial gradients
Climate velocities now point in the expected direction

fun_calc_var.py
@@ -134,6 +134,33 @@ def calc_climate_speed(darr, setDict):
     # ic(check_stats(climSpd.data))
     
     return climSpd
+    
+def calc_spat_grad(darr, setDict):
+    ''' Calculate the climate speed of a variable '''
+    tGrad, nsGrad, ewGrad, yrSpan = calc_spat_temp_grad(darr, setDict)  
+    totGrad = np.sqrt((nsGrad ** 2) + (ewGrad ** 2))
+    sGradDs = xr.Dataset(
+        data_vars = dict(
+            ns=(["lat","lon"], nsGrad),
+            ew=(["lat","lon"], ewGrad),
+        ),
+        coords={
+            "lat": tGrad['lat'],
+            "lon": tGrad['lon']
+        }
+    )
+    sGradDs.attrs = darr.attrs
+    sGradDs['ns'].attrs['long_name'] = 'Spatial grad of 2m temp (N-S)'
+    sGradDs['ns'].attrs['scenario'] = darr.scenario
+    sGradDs['ns'].attrs['units'] = 'C/km'
+    sGradDs['ew'].attrs['long_name'] = 'Spatial grad of 2m temp (E-W)'
+    sGradDs['ew'].attrs['scenario'] = darr.scenario
+    sGradDs['ew'].attrs['units'] = 'C/km'
+    sGradDs.attrs['scenario'] = darr.scenario
+
+    sGradDsToPlot = sGradDs['ew']
+    
+    return sGradDsToPlot
 
 def calc_decadal_climate_distance(darr, setDict):
     ''' Calculate the decadal climate distance of a variable '''
@@ -147,17 +174,16 @@ def calc_decadal_climate_distance(darr, setDict):
     dcd.attrs['units'] = 'km'
 
     return dcd
-                                    
+
 def calc_climate_velocity(darr, setDict):
     ''' Calculate the climate velocity (vector form) of a variable '''
     tGrad, nsGrad, ewGrad, yrsSpan = calc_spat_temp_grad(darr, setDict)
-    nsClimVel = tGrad / nsGrad
-    ewClimVel = tGrad / ewGrad
+    nsClimVel = -1 * tGrad / nsGrad
+    ewClimVel = -1 * tGrad / ewGrad
     # ewGrad, nsGrad
     totGrad = np.sqrt((nsGrad ** 2) + (ewGrad ** 2))
-    sGrad = totGrad   
+    sGrad = totGrad
     climSpd = tGrad / sGrad
-    
     climVelDs = xr.Dataset(
         data_vars = dict(
             ns=(["realization","lat","lon"], nsClimVel.data),
@@ -165,11 +191,23 @@ def calc_climate_velocity(darr, setDict):
             cspd=(["realization","lat","lon"], climSpd.data)
         ),
         coords={
-            "realization": tGrad['realization'].data,
-            "lat": tGrad['lat'],
-            "lon": tGrad['lon']
+            "realization": darr['realization'].data,
+            "lat": darr['lat'],
+            "lon": darr['lon']
         }
     )
+       
+    # climVelDs = xr.Dataset(
+    #     data_vars = dict(
+    #         ns=(["lat","lon"], nsClimVel.data),
+    #         ew=(["lat","lon"], ewClimVel.data),
+    #         cspd=(["lat","lon"], climSpd.data)
+    #     ),
+    #     coords={
+    #         "lat": tGrad['lat'].data,
+    #         "lon": tGrad['lon'].data
+    #     }
+    # )
     climVelDs['ns'].attrs['long_name'] = 'Climate velocity of 2m temp (N-S)'
     climVelDs['ns'].attrs['scenario'] = darr.scenario
     climVelDs['ns'].attrs['units'] = 'km/yr'
@@ -182,6 +220,42 @@ def calc_climate_velocity(darr, setDict):
     climVelDs.attrs['scenario'] = darr.scenario
 
     return climVelDs
+
+                                                                                                            
+# def calc_climate_velocity(darr, setDict):
+#     ''' Calculate the climate velocity (vector form) of a variable '''
+#     tGrad, nsGrad, ewGrad, yrsSpan = calc_spat_temp_grad(darr, setDict)
+#     nsClimVel = tGrad / nsGrad
+#     ewClimVel = tGrad / ewGrad
+#     # ewGrad, nsGrad
+#     totGrad = np.sqrt((nsGrad ** 2) + (ewGrad ** 2))
+#     sGrad = totGrad   
+#     climSpd = tGrad / sGrad
+    
+#     climVelDs = xr.Dataset(
+#         data_vars = dict(
+#             ns=(["realization","lat","lon"], nsClimVel.data),
+#             ew=(["realization","lat","lon"], ewClimVel.data),
+#             cspd=(["realization","lat","lon"], climSpd.data)
+#         ),
+#         coords={
+#             "realization": tGrad['realization'].data,
+#             "lat": tGrad['lat'],
+#             "lon": tGrad['lon']
+#         }
+#     )
+#     climVelDs['ns'].attrs['long_name'] = 'Climate velocity of 2m temp (N-S)'
+#     climVelDs['ns'].attrs['scenario'] = darr.scenario
+#     climVelDs['ns'].attrs['units'] = 'km/yr'
+#     climVelDs['ew'].attrs['long_name'] = 'Climate velocity of 2m temp (E-W)'
+#     climVelDs['ew'].attrs['scenario'] = darr.scenario
+#     climVelDs['ew'].attrs['units'] = 'km/yr'
+#     climVelDs['cspd'].attrs['long_name'] = 'Climate speed of 2m temp'
+#     climVelDs['cspd'].attrs['scenario'] = darr.scenario
+#     climVelDs['cspd'].attrs['units'] = 'km/yr'
+#     climVelDs.attrs['scenario'] = darr.scenario
+
+#     return climVelDs
                                     
 def calc_seasonal_shift(darr, setDict):
     ''' Calculate seasonal shift of a variable '''

fun_plot_slice_globe.py
@@ -58,16 +58,19 @@ def plot_single_slice_globe(rlzList, dataDict, setDict, outDict):
         sys.exit('Check plotPanel input')
 
     # Obtain ens mean, pointwise max/min, or individual realization
-    if setDict["plotEnsType"] == 'mean': #Ensemble mean
-        panel = actData.mean(dim='realization')
-    elif setDict["plotEnsType"] == 'max': #Pointwise max
-        panel = actData.max(dim='realization')
-    elif setDict["plotEnsType"] == 'min': #Pointwise min
-        panel = actData.min(dim='realization')
-    elif isinstance(setDict["plotEnsType"], int): #Single member
-        panel = actData.isel(realization=setDict["plotEnsType"])
-    else:
-        sys.exit('Check plotEnsType input!')
+    try:
+        if setDict["plotEnsType"] == 'mean': #Ensemble mean
+            panel = actData.mean(dim='realization')
+        elif setDict["plotEnsType"] == 'max': #Pointwise max
+            panel = actData.max(dim='realization')
+        elif setDict["plotEnsType"] == 'min': #Pointwise min
+            panel = actData.min(dim='realization')
+        elif isinstance(setDict["plotEnsType"], int): #Single member
+            panel = actData.isel(realization=setDict["plotEnsType"])
+        else:
+            sys.exit('Check plotEnsType input!')
+    except:
+        panel = actData
         
     panelStr = setDict["plotPanel"]
 
@@ -86,10 +89,10 @@ def plot_single_slice_globe(rlzList, dataDict, setDict, outDict):
     plt.rcParams.update({'font.weight': 'light'}) #normal, bold, heavy, light, ultrabold, ultralight
     fpt.drawOnGlobe(
         ax, panel, lats, lons, cmap, vmin=cbVals[0], vmax=cbVals[1],
-        cbarBool=False, fastBool=True, extent='both',
+        cbarBool=True, fastBool=True, extent='both',
         addCyclicPoint=setDict["addCyclicPoint"], alph=1)
 
-    savePrfx = '' #Easy modification for unique filename
+    savePrfx = 'SPATGRADEW_' #Easy modification for unique filename
     saveStr = panelStr + '_' + md["varSve"] + '_' \
         + 'rlz' + str(setDict["plotEnsType"])
     savename = outDict["savePath"] + savePrfx + saveStr + '.png'
@@ -123,16 +126,19 @@ def plot_single_slice_vector_globe(rlzList, dataDict, setDict, outDict):
     # Obtain ens mean, pointwise max/min, or individual realization
     panel = dict()
     for varKey in ['ns', 'ew', 'cspd']:
-        if setDict["plotEnsType"] == 'mean': #Ensemble mean
-            panel[varKey] = actData[varKey].mean(dim='realization')
-        elif setDict["plotEnsType"] == 'max': #Pointwise max
-            panel[varKey] = actData[varKey].max(dim='realization')
-        elif setDict["plotEnsType"] == 'min': #Pointwise min
-            panel[varKey] = actData[varKey].min(dim='realization')
-        elif isinstance(setDict["plotEnsType"], int): #Single member
-            panel[varKey] = actData[varKey].isel(realization=setDict["plotEnsType"])
-        else:
-            sys.exit('Check plotEnsType input!')
+        try:
+            if setDict["plotEnsType"] == 'mean': #Ensemble mean
+                panel[varKey] = actData[varKey].mean(dim='realization')
+            elif setDict["plotEnsType"] == 'max': #Pointwise max
+                panel[varKey] = actData[varKey].max(dim='realization')
+            elif setDict["plotEnsType"] == 'min': #Pointwise min
+                panel[varKey] = actData[varKey].min(dim='realization')
+            elif isinstance(setDict["plotEnsType"], int): #Single member
+                panel[varKey] = actData[varKey].isel(realization=setDict["plotEnsType"])
+            else:
+                sys.exit('Check plotEnsType input!')
+        except:
+            panel[varKey] = actData[varKey]
         
     panelStr = setDict["plotPanel"]
     # ic(panel)
@@ -141,6 +147,7 @@ def plot_single_slice_vector_globe(rlzList, dataDict, setDict, outDict):
     CL = 0.
     import cartopy as ct
     mapProj = ccrs.EqualEarth(central_longitude = CL)
+    # mapProj = ccrs.Orthographic(180, -90)
     fig = plt.figure(figsize=(12, 2.73*2))
     ax = plt.subplot(1, 1, 1, projection=mapProj) #nrow ncol index
     plt.rcParams.update({'font.family': 'Open Sans'})
@@ -160,28 +167,18 @@ def plot_single_slice_vector_globe(rlzList, dataDict, setDict, outDict):
     
     zmzmDisc = fpt.get_cspd_colormap('zmzmGray')
     archer = ax.quiver(
-        lons, lats, nsDataNorm, ewDataNorm, cspd, scale=200, transform=data_crs,
+        lons, lats, ewDataNorm, nsDataNorm, cspd, scale=200, transform=data_crs,
         cmap=zmzmDisc, headwidth=2, headlength=5, headaxislength=4.5)
         #headwidth=3, headlength=5, headaxislength=4.5 default
-    plt.colorbar(archer, cmap=zmzmDisc)
+        #'boid' aesthetics are headwidth=2, headlength=5, headaxislength=4.5
+    plt.colorbar(archer, cmap=zmzmDisc, ticks=[-50,-30,-10,-2,2,10,30,50])
     archer.set_clim(-51, 51)
-    # cmap = cmocean.cm.balance if setDict["cmap"] is None else setDict["cmap"]
-
-    # cbAuto = np.sort(
-    #     [-np.nanquantile(panel.data,0.75), np.nanquantile(panel.data,0.75)])
-    # cbVals = cbAuto if setDict["cbVals"] is None else setDict["cbVals"]
-    # md = fpd.meta_book(setDict, dataDict, panel)
-
-    # fpt.drawOnGlobe(
-    #     ax, panel, lats, lons, cmap, vmin=cbVals[0], vmax=cbVals[1],
-    #     cbarBool=False, fastBool=True, extent='both',
-    #     addCyclicPoint=setDict["addCyclicPoint"], alph=1)
 
     savePrfx = '' #Easy modification for unique filename
     # saveStr = panelStr + '_' + md["varSve"] + '_' \
     #     + 'rlz' + str(setDict["plotEnsType"])
     # savename = outDict["savePath"] + savePrfx + saveStr + '.png'
-    savename = outDict["savePath"] + '24_quiver_headwidth500dpi' + '.png'
+    savename = outDict["savePath"] + '4_quiver_climVel_UKESMS245_20352044' + '.png'
     plt.savefig(savename, dpi=outDict["dpiVal"], bbox_inches='tight')
     plt.close()
     ic(savename)